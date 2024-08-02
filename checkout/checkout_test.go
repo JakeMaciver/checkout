442906296b4120f7a4387393fc2903a6cc8656c4
Jake Maciver <jacobmaciverwork@gmail.co.uk>
2024-08-02 21:24:51 +0100

    start building Scan method test

--- checkout/checkout_test.go
+++ checkout/checkout_test.go
@@ -1,5 +1,19 @@
 package checkout_test
 
-// test Scan
+import (
+	"testing"
 
+	"github.com/JakeMaciver/checkout/pricing"
+)
+
+// test Scan
+func TestScan(t *testing.T) {
+
+	prices := map[string]pricing.ItemPricing{
+		"A": {UnitPrice: 50, SpecialQty: 3, SpecialPrice: 130},
+		"B": {UnitPrice: 30, SpecialQty: 2, SpecialPrice: 45},
+		"C": {UnitPrice: 20, SpecialQty: 0, SpecialPrice: 0},
+		"D": {UnitPrice: 15, SpecialQty: 0, SpecialPrice: 0},
+	} 
+}
 // test GetTotal
