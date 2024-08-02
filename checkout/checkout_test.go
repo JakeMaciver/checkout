912ae0fdc758ebad6344c6df25b3429366e1ba35
Jake Maciver <jacobmaciverwork@gmail.co.uk>
2024-08-02 22:16:52 +0100

    setup for prices, catalogue and chackout is the same

--- checkout/checkout_test.go
+++ checkout/checkout_test.go
@@ -71,6 +71,16 @@ func TestScan(t *testing.T) {
 	})
 
 	t.Run("error case, not found", func(t *testing.T) {
+		prices := map[string]pricing.ItemPricing{
+			"A": {NormalPrice: 50, SpecialQty: 3, SpecialPrice: 130},
+			"B": {NormalPrice: 30, SpecialQty: 2, SpecialPrice: 45},
+			"C": {NormalPrice: 20, SpecialQty: 0, SpecialPrice: 0},
+			"D": {NormalPrice: 15, SpecialQty: 0, SpecialPrice: 0},
+		}
+	
+		catalogue := pricing.NewCatalogue(prices)
+		checkout := checkout.NewCheckout(*catalogue)
+
 		
 	})
 }
