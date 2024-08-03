a3e78e9abd668fc1d0b623a63b29197cc4ecfb9f
Jake Maciver <jacobmaciverwork@gmail.co.uk>
2024-08-03 10:38:44 +0100

    flush out test to invoke AddItem method. checks if SKU is present in map. checks if item pricings are the same. test fails as not implemented method yet

--- pricing/pricing_test.go
+++ pricing/pricing_test.go
@@ -1,6 +1,7 @@
 package pricing_test
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/JakeMaciver/checkout/pricing"
@@ -14,7 +15,28 @@ func TestAddItem(t *testing.T) {
 
 	catalogue := pricing.NewCatalogue(prices)
 
-	
+	SKUtoAdd := "B"
+	newItem := &pricing.ItemPricing{
+		NormalPrice: 20,
+		SpecialQty: 3,
+		SpecialPrice: 40,
+	}
+
+	catalogue.AddItem(SKUtoAdd, newItem.NormalPrice, newItem.SpecialQty, newItem.SpecialPrice)
+
+	_, got := catalogue.Prices[SKUtoAdd]
+	want := true
+
+	if got != want {
+		t.Errorf("got: %v, want: %v", got, want)
+	}
+
+	gotItem := catalogue.Prices[SKUtoAdd]
+	wantItem := newItem
+
+	if !reflect.DeepEqual(gotItem, wantItem) {
+		t.Errorf("got: %v, want: %v", got, want)
+	}
 }
 
 // Test for updating an item in the catalogue
