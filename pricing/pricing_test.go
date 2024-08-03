47f6b6f98377e36a80454b48f6c4c984f160b1f9
Jake Maciver <jacobmaciverwork@gmail.co.uk>
2024-08-03 10:27:26 +0100

    start first test (add item) by initialising a catalogue

--- pricing/pricing_test.go
+++ pricing/pricing_test.go
@@ -1,5 +1,21 @@
 package pricing_test
 
+import (
+	"testing"
+
+	"github.com/JakeMaciver/checkout/pricing"
+)
+
 // Test for adding an item to the catalogue
+func TestAddItem(t *testing.T) {
+	prices := map[string]pricing.ItemPricing{
+		"A": {NormalPrice: 50, SpecialQty: 3, SpecialPrice: 130},
+	}
+
+	catalogue := pricing.NewCatalogue(prices)
+
+	
+}
+
 // Test for updating an item in the catalogue
 // Test for deleting an item in the catalogue
