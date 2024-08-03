202774bc07619629f22858c61d47c353a0ba4edb
Jake Maciver <jacobmaciverwork@gmail.co.uk>
2024-08-03 14:07:45 +0100

    create test for an UpdateItem method for the pricing package. test invokes UpdateItem after making a catalogue, test fails

--- pricing/pricing_test.go
+++ pricing/pricing_test.go
@@ -130,5 +130,20 @@ func TestAddItem(t *testing.T) {
 }
 
 // Test for updating an item in the catalogue
+func TestUpdateItem(t *testing.T) {
+	prices := map[string]pricing.ItemPricing{
+		"A": {NormalPrice: 50, SpecialQty: 3, SpecialPrice: 130},
+	}
 
+	catalogue := pricing.NewCatalogue(prices)
+
+	SKUtoUpdate := "A"
+	newItem := pricing.ItemPricing{
+		NormalPrice:  30,
+		SpecialQty:   3,
+		SpecialPrice: 40,
+	}	
+
+	catalogue.UpdateItem(SKUtoUpdate, newItem.NormalPrice, newItem.SpecialQty, newItem.SpecialPrice)
+}
 // Test for deleting an item in the catalogue
